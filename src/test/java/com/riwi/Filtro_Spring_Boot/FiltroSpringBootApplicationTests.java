585589d18737a74adb9dfb57cd0575b782fdca91
Alejo Perez <alejo.perez2905@gmail.com>
2024-06-04 06:27:17 -0500

    Se inicia el filtro

---
+++ src/test/java/com/riwi/Filtro_Spring_Boot/FiltroSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.Filtro_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltroSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
