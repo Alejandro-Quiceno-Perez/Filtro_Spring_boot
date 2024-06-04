dc6a0fa04b8fd76e965d4a8c2a2f74974870bba8
Alejo Perez <alejo.perez2905@gmail.com>
2024-06-04 08:40:47 -0500

    DTOS

---
+++ src/main/java/com/riwi/Filtro_Spring_Boot/api/dto/response/UserResponse.java
@@ -0,0 +1,14 @@
+package com.riwi.Filtro_Spring_Boot.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    
+}
